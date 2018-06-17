8096434419db2c76c85c73ff43fc2a509c6cda2c
Raghuttam Hombal <group4pgs@gmail.com>
2018-06-17 18:54:15 +0530

    Update photoresistorTrial.ino

--- photoresistorTrial.ino
+++ photoresistorTrial.ino
@@ -1,12 +1,13 @@
-int photoPin = 11;
+//This file gives the instantaneous voltage values provided by the photoresistor every second in the Serial O/P
+
+int photoPin = 11; //The photoresistor is connected to the Pin11 to get Analog input
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); 
   pinMode(photoPin,INPUT);
 }
 
 void loop() {
   int light = analogRead(photoPin);
   Serial.println(light);
-  delay(1000);
+  delay(1000); // Wait for a second
 }
