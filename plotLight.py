3e535ae2d83df5ee47e17659c50981a20ef09dd9
Raghuttam Hombal <group4pgs@gmail.com>
2018-06-17 19:00:00 +0530

    Update plotLight.py

--- plotLight.py
+++ plotLight.py
@@ -1,32 +1,23 @@
 
 # coding: utf-8
 
-# In[2]:
-
 
 import serial
 import matplotlib.pyplot as plt
-from drawnow import *
-
-
-# In[3]:
-
+from drawnow import * #Used to plot live data
 
 def plot_func():
     plt.title("Light")
-    plt.ylim(500,800)
+    plt.ylim(500,800) #Can vary depending upon the resistor value
     plt.xlabel("Time (sec)")
     plt.plot(light)
     plt.show()
 
 
-# In[5]:
-
-
-arduino = serial.Serial('/dev/ttyACM0',9600)
-plt.ion()
+arduino = serial.Serial('/dev/ttyACM0',9600) #Creating an object to Fetch the value from Serial Port
+plt.ion()                                    #Used to plot live data in a single frame
 cnt=0
-light=[]
+light=[]                                    #Where the data is stored
 while True:
     if arduino.inWaiting()>1:
         myData = arduino.readline()
@@ -34,6 +25,6 @@ while True:
         light.append(myData)
         drawnow(plot_func)
         cnt = cnt+1
-        if cnt>50:
-            light.pop(0)
+        if cnt>50:                          #Can change the number depending upon the number of values you want to be plotted
+            light.pop(0)                    #Removing the previous value to keep the plot till 50 values
 
